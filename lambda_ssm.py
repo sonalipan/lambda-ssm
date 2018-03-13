45db56bfed6b333eb837c30a776d33880d4db6b4
Sonali Panda <sonalipan@cybage.com>
2018-03-13 14:14:28 +0530

    added

---
+++ lambda_ssm.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+import boto3
+import time
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    message = event['Records'][0]['Sns']['Message']
+    #print("From SNS: " + message)
+    instanceId = 'i-0d40bf0f0d09b981c'
+    if (message=='start') :
+        ssmClient = boto3.client('ssm')
+        ssmCommand = ssmClient.send_command(
+        InstanceIds = [
+           instanceId
+        ],
+        DocumentName = 'AWS-RunShellScript',
+        TimeoutSeconds = 240,
+        Comment = 'Run nginx',
+        Parameters = {
+           'commands': [
+              "sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT"
+          ]
+        }
+    )
+    else :
+        ssmClient = boto3.client('ssm')
+        ssmCommand = ssmClient.send_command(
+        InstanceIds = [
+           instanceId
+        ],
+        DocumentName = 'AWS-RunShellScript',
+        TimeoutSeconds = 240,
+        Comment = 'Donot Run Tests',
+        Parameters = {
+           'commands': [
+             "sudo iptables -I INPUT -p tcp --dport 80 -j DROP"
+          ]
+        }
+    )
+    print("From SNS: " + message)
